ba256b54d57185116b1b8e443c36a259472500ed
Neptune <neptunecurrency@gmail.com>
2022-10-08 11:17:02 +0530

    config

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -69,16 +69,6 @@ const config: (t: ContextApi['t'], isDark: boolean, languageCode?: string, chain
         ].map((item) => addMenuItemSupported(item, chainId)),
       },
       {
-        label: t('Arbitrage (coming soon)'),
-        href: '/arbitrage',
-        supportChainIds: SUPPORT_ONLY_BSC,
-      },
-      {
-        label: t('FlashLoans (coming soon)'),
-        href: '/flashloans',
-        supportChainIds: SUPPORT_ONLY_BSC,
-      },
-      {
         label: '',
         icon: MoreIcon,
         hideSubNav: true,
